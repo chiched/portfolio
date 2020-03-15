98ea447799d5769da42e08be92375f61dc438723
David Chiche <davidchiche@gmail.com>
2020-03-15 23:37:49 +0900

    Added styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,4 @@
 import React from "react"
-
 export default () => (
-  <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
 )
